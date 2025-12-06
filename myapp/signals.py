d191f9cfc9addd2b23112c42b02c8c67ba344f7e
batoul-m <batoul.2003.m@gmail.com>
2025-12-06 21:29:06 +0200

    Make main branch empty for initial PR

--- myapp/signals.py
+++
@@ -1,7 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from myapp.models import MyModel
-
-@receiver(post_save, sender=MyModel)
-def my_handler(sender, instance, created, **kwargs):
-    print("Model was saved!")
